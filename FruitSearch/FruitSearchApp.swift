6d1108a3a10b58a19600c6c065f362435b9a0c5e
Marcel Jäger <marcel.jaeger.a@outlook.com>
2024-07-16 10:20:15 +0200

    Initial Commit

---
+++ FruitSearch/FruitSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitSearchApp.swift
+//  FruitSearch
+//
+//  Created by Marcel Jäger on 16.07.24.
+//
+
+import SwiftUI
+
+@main
+struct FruitSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
