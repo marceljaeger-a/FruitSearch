67d4dfebdb709d393912e34453ca6dda6157026a
Marcel Jäger <marcel.jaeger.a@outlook.com>
2024-07-31 11:02:42 +0200

    Add JSON file for registering IngredientIDs

--- FruitSearch/Spoonacular API/IngredientID.swift
+++ FruitSearch/Spoonacular API/IngredientID.swift
@@ -8,20 +8,45 @@
 import Foundation
 
 struct IngredientID: Codable {
+    var name: String
     var value: Int
     
     static func get(of name: String) -> Self? {
-        switch name {
-        case "Apple":
-            return .init(value: 9003)
-        case "Banana":
-            return .init(value: 9040)
-        case "Orange":
-            return .init(value: 9200)
-        case "Tomato":
-            return .init(value: 11529)
-        default:
+        do {
+            let savedIdentifiers = try Self.loadData()
+            return savedIdentifiers.first(where: { $0.name.lowercased() == name.lowercased() })
+        } catch {
+            print(error)
             return nil
         }
     }
 }
+
+extension IngredientID {
+    enum IngredientIDError: Error {
+        case invalidFileURL
+        case invalidFileContent
+        case invalidData
+    }
+    
+    private static func loadData() throws -> Array<Self> {
+        let url = Bundle.main.url(forResource: "IngredientIDs", withExtension: "json")
+        guard let url else { throw IngredientIDError.invalidFileURL }
+        
+        let content = FileManager.default.contents(atPath: url.path())
+        guard let content else { throw IngredientIDError.invalidFileContent }
+        
+        let decoder = JSONDecoder()
+        let ids = try? decoder.decode(Array<Self>.self, from: content)
+        guard let ids else { throw IngredientIDError.invalidData }
+        
+        return ids
+    }
+    
+//    private static func saveData(_ data: Array<Self>) throws {
+//        
+//    }
+}
+
+
+
